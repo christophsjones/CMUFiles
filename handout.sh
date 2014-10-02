af29f674b7068cd1ec18710317f916511db096ab
Chris Jones <csj@andrew.cmu.edu>
2014-10-02 13:01:26 -0400

    Warns you if tmp directory exists already

--- handout.sh
+++ handout.sh
@@ -2,11 +2,16 @@
 
 USER=csj
 
+if [ -d tmp ]
+then
+    echo "tmp directory already exists. Please remove it"
+    exit 1
+fi
+
 mkdir tmp
 scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/$USER/* ./tmp
 for f in tmp/*
 do
     cat $f
 done
-rm tmp/*
-rmdir tmp
+rm -r tmp
