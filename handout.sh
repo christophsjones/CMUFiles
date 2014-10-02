e27997555a83949678d5443cefc23d22e439ed87
Chris Jones <csj@andrew.cmu.edu>
2014-10-02 12:57:55 -0400

    Handout no longer needs hw number

--- handout.sh
+++ handout.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 USER=csj
-USAGE="usage: handout <hw_num>"
 
-if [ $# -eq 0 ]
-    then
-        echo $USAGE
-        exit 1
-fi
-
-scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/$USER/hw$1 ./hw$1_grades
-cat ./hw$1_grades
-rm ./hw$1_grades
+mkdir tmp
+scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/$USER/* ./tmp
+for f in tmp/*
+do
+    cat $f
+done
+rm tmp/*
+rmdir tmp
