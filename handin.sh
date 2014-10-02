9bd9f9e7c4c931a61772229d76cbb6fff4fc7bdc
Chris Jones <csj@andrew.cmu.edu>
2014-10-02 02:30:29 -0400

    Fixed handin

--- handin.sh
+++ handin.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Usage: ./handin.sh ##, where hw##.pdf is the name of the file to handin.
-USER=icw
-cp hw$1.pdf /afs/andrew.cmu.edu/course/15/354/handin/HW$1/$USER/hw$1.pdf
+
+USER=csj
+USAGE="usage: handin <hw_num>"
+
+if [ $# -eq 0 ]
+then
+    echo $USAGE
+    exit 1
+fi
+
+cp hw$1/hw$1.pdf /afs/andrew.cmu.edu/course/15/354/handin/HW$1/$USER/hw$1.pdf
