6c9b55b6d1c9da52a883c922be2538d30455b8f0
Chris Jones <csj@andrew.cmu.edu>
2014-11-04 18:30:01 -0500

    Now reads mean, median, stddev scores as well as your own

--- handout.sh
+++ handout.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-USER=csj
+USER="csj"
+USAGE="usage: handout <hw_num>"
+
+if [ $# -eq 0 ]
+then
+    echo $USAGE
+    exit 1
+fi
 
 if [ -d tmp ]
 then
@@ -9,9 +16,17 @@ then
 fi
 
 mkdir tmp
-scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/$USER/* ./tmp
-for f in tmp/*
-do
-    cat $f
-done
+scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/$USER/hw0$1 ./tmp/me
+echo "Your score:"
+cat tmp/me
+scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/mean/hw0$1 ./tmp/mean
+echo "Mean:"
+cat tmp/mean
+scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/median/hw0$1 ./tmp/med
+echo "Median:"
+cat tmp/med
+scp $USER@unix.andrew.cmu.edu:/afs/andrew.cmu.edu/course/15/354/handout/stddev/hw0$1 ./tmp/dev
+echo "Std deviation:"
+cat tmp/dev
+
 rm -r tmp
